895fa1981925bedc7d4a48f5d2b51bb0b50d222f
Jina Ham <hamjina61@gmail.com>
2025-06-15 14:14:17 +0900

    feat: 게시글 CRUD 구현

---
+++ service/article/src/main/java/high/traffic/forum/article/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package high.traffic.forum.article.repository;
+
+import high.traffic.forum.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
---
+++ service/article/src/main/java/high/traffic/forum/article/service/ArticleService.java
@@ -0,0 +1,43 @@
+package high.traffic.forum.article.service;
+
+import high.traffic.forum.article.entity.Article;
+import high.traffic.forum.article.repository.ArticleRepository;
+import high.traffic.forum.article.service.request.ArticleCreateRequest;
+import high.traffic.forum.article.service.request.ArticleUpdateRequest;
+import high.traffic.forum.article.service.response.ArticleResponse;
+import jakarta.transaction.TransactionScoped;
+import jakarta.transaction.Transactional;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public ArticleResponse create(ArticleCreateRequest request) {
+        Article article = articleRepository.save(
+                Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
+        );
+        return ArticleResponse.from(article);
+    }
+
+    @Transactional
+    public ArticleResponse update(Long articleId, ArticleUpdateRequest request) {
+        Article article = articleRepository.findById(articleId).orElseThrow();
+        article.update(request.getTitle(), request.getContent());
+        return ArticleResponse.from(article);
+    }
+
+    public ArticleResponse read(Long articleId) {
+        return ArticleResponse.from(articleRepository.findById(articleId).orElseThrow());
+    }
+
+    @Transactional
+    public void delete(Long articleId) {
+        articleRepository.deleteById(articleId);
+    }
+}
