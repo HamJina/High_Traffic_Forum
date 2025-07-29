210843bb6613604e7b9cadb5e427b6010f84bbf7
Jina Ham <hamjina61@gmail.com>
2025-07-29 15:31:16 +0900

    feat: 조회수 어뷰징 방지 정책 추가 구현

--- service/view/src/main/java/high/traffic/forum/view/service/ArticleViewService.java
+++ service/view/src/main/java/high/traffic/forum/view/service/ArticleViewService.java
@@ -1,17 +1,26 @@
 package high.traffic.forum.view.service;
 
 import high.traffic.forum.view.repository.ArticleViewCountRepository;
+import high.traffic.forum.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
     private final ArticleViewCountRepository articleViewCountRepository;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
     private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if(!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if(count % BACK_UP_BACH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
